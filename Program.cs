592661e5bcb26a17ec870e30a2cc21c061dea93c
yosshii50 <kasaubunts@gmail.com>
2020-10-17 15:03:20 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,26 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
 
 namespace SoundOn_Off
 {
     class Program
     {
 
-        [DllImport("user32.dll")]
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
         const int WM_APPCOMMAND = 0x319;
 
         const int APPCOMMAND_VOLUME_MUTE = 0x80000;
-        const int APPCOMMAND_VOLUME_DOWN = 0x90000;
-        const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        //const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        //const int APPCOMMAND_VOLUME_UP = 0xA0000;
 
         static void Main(string[] args)
         {
-            IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+            IntPtr hWnd = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
             SendMessage(hWnd, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_MUTE);
         }
     }
