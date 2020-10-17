a99b6471dc0d228c64273272a6b67445c2b51668
yosshii50 <kasaubunts@gmail.com>
2020-10-17 14:51:55 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace SoundOn_Off
+{
+    class Program
+    {
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        const int WM_APPCOMMAND = 0x319;
+
+        const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        const int APPCOMMAND_VOLUME_UP = 0xA0000;
+
+        static void Main(string[] args)
+        {
+            IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+            SendMessage(hWnd, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+    }
+}
